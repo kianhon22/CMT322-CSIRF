72bb0d3ed0f7ec98441d27a344886ec3908658f6
Tan Kian Hon <tkianhon@gmail.com>
2024-11-23 17:00:19 +0800

    amend footer and navigation css

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,73 +1,73 @@
 <template>
     <footer class="bg-black dark:bg-gray-900">
-        <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-            <div class="md:flex md:justify-between">
-            <div class="mb-6 md:mb-0">
-                <a href="/" class="flex items-center">
-                    <img src="/csirf-logo.png" class="h-8 me-3" alt="CSIRF Logo" />
-                    <span class="text-white self-center text-2xl font-semibold whitespace-nowrap">CSIRF</span>
-                </a>
-            </div>
-            <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                <div>
-                    <h2 class="mb-6 text-lg font-semibold text-white uppercase">Navigation</h2>
-                    <ul class="text-white text-lg">
-                        <li class="mb-4">
-                            <a href="https://flowbite.com/" class="hover:underline">Home</a>
-                        </li>
-                        <li>
-                            <a href="https://tailwindcss.com/" class="hover:underline">Company</a>
-                        </li>
-                    </ul>
+        <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-6">
+            <div class="md:flex md:justify-evenly items-center">
+                <div class="mb-6 md:mb-0 text-center md:text-left">
+                    <a href="/" class="flex items-center md:items-start">
+                        <img src="/csirf-logo.png" class="h-24 mb-2" alt="CSIRF Logo" />
+                        <span class="ml-4 text-white self-center text-4xl font-semibold whitespace-nowrap">CSIRF</span>
+                    </a>
                 </div>
-                <div>
-                    <h2 class="mb-6 text-lg font-semibold text-white uppercase">Contact Us</h2>
-                    <ul class="text-white text-lg">
-                        <li class="mb-4">
-                            csirf@usm.my
-                        </li>
-                        <li>
-                            013-6630579
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-8 text-lg font-semibold text-white uppercase">Social Media</h2>
-                        <div class="flex mt-4 sm:justify-center sm:mt-0">
-                    <a href="#" class="text-white hover:text-gray-900">
-                        <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                            <path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd"/>
-                        </svg>
-                    </a>
-                    <a href="#" class="text-white hover:text-gray-900 ms-5">
-                        <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
-                        </svg>
-                    </a>
-                    <a href="#" class="text-white hover:text-gray-900 ms-5">
-                        <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                            <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/><path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
-                        </svg>
-                    </a>
-                    <a href="#" class="text-white hover:text-gray-900 ms-5">
-                        <svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M18.942 5.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.586 11.586 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3 17.392 17.392 0 0 0-2.868 11.662 15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.638 10.638 0 0 1-1.706-.83c.143-.106.283-.217.418-.331a11.664 11.664 0 0 0 10.118 0c.137.114.277.225.418.331-.544.328-1.116.606-1.71.832a12.58 12.58 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM8.678 14.813a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.929 1.929 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/>
-                        </svg>
-                    </a>
+                <div class="grid grid-cols-1 sm:grid-cols-3 gap-8">
+                    <div class="mx-8">
+                        <h2 class="mb-6 text-lg font-semibold text-white uppercase">Navigation</h2>
+                        <ul class="text-white text-lg">
+                            <li class="mb-4">
+                                <a href="https://flowbite.com/" class="rounded px-2 py-0.5 hover:text-orange-500">Home</a>
+                            </li>
+                            <li>
+                                <a href="https://tailwindcss.com/" class="rounded px-2 py-0.5 hover:text-orange-500">Company</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mx-8">
+                        <h2 class="mb-6 text-lg font-semibold text-white uppercase">Contact Us</h2>
+                        <ul class="text-white text-lg">
+                            <li class="mb-4">
+                                <a href="mailto:csirf@usm.my" class="hover:text-orange-500">csirf@usm.my</a>
+                            </li>
+                            <li>
+                                <a href="https://wa.me/60136630579" class="hover:text-orange-500" target="_blank">013-6630579</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mx-8">
+                        <h2 class="mb-6 text-lg font-semibold text-white uppercase">Social Media</h2>
+                        <div class="grid grid-cols-2 gap-4">
+                            <a href="https://www.facebook.com/profile.php?id=61552370731721" target="_blank" class="text-white hover:text-orange-500 flex justify-center">
+                                <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd"/>
+                                </svg>
+                            </a>
+                            <a href="https://www.instagram.com/vcsirf_usm" target="_blank" class="text-white hover:text-orange-500 flex justify-center">
+                                <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
+                                </svg>
+                            </a>
+                            <a href="https://www.linkedin.com/company/virtualcsirfusm" target="_blank" class="text-white hover:text-orange-500 flex justify-center">
+                                <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/><path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
+                                </svg>
+                            </a>
+                            <a href="https://discordapp.com/users/tkh0622" target="_blank" class="text-white hover:text-orange-500 flex justify-center">
+                                <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                    <path d="M18.942 5.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.586 11.586 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3 17.392 17.392 0 0 0-2.868 11.662 15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.638 10.638 0 0 1-1.706-.83c.143-.106.283-.217.418-.331a11.664 11.664 0 0 0 10.118 0c.137.114.277.225.418.331-.544.328-1.116.606-1.71.832a12.58 12.58 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM8.678 14.813a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.929 1.929 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/>
+                                </svg>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <div class="sm:flex sm:items-center sm:justify-between">
+        <hr class="border-gray-200 sm:mx-auto" />
+        <div class="my-4 mx-6 sm:flex sm:items-center sm:justify-between">
             <span class="text-lg text-white sm:text-center">© 2024
-                <a href="https://cssocietyusm.com/" class="hover:underline">Computer Science Society</a>
+                <a href="https://cssocietyusm.com/" class="hover:text-orange-500">Computer Science Society</a>
                 @ Universiti Sains Malaysia
             </span>
             <span class="text-lg text-white sm:text-center">
                 All Rights Reserved.
             </span>
-        </div>
         </div>
     </footer>
 </template>
