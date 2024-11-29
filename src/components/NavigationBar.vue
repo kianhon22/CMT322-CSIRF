f5c746e09a0b5e73d01bb89fb1dfb0b3fcbae32f
Huey Jing Yeoh <93303566+yeoh0730@users.noreply.github.com>
2024-11-29 14:53:32 +0800

    update footer, company

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -45,13 +45,6 @@
                   > Career
                   </router-link>
                 </li>
-                <li>
-                  <router-link to="/company"
-                    class="block px-4 py-2 lg:px-2 lg:py-1 pr-4 pl-3 text-white border-b border-gray-100 rounded-lg lg:border-0 lg:hover:bg-orange-500 hover:text-[#1E1B4B]"
-                    active-class="!bg-orange-500 !text-[#1E1B4B]"
-                  > Company
-                  </router-link>
-                </li>
                 <div v-if="currentUser && (currentUser.role=='sponsor' || currentUser.role=='student')">
                   <li>
                     <router-link to="/profile"
