07bcbd750f266ff93b9dc81af3a7b7a6906b02e9
Peiyinzz <117030605+Peiyinzz@users.noreply.github.com>
2024-12-06 00:23:25 +0800

    itinerary all button

--- src/components/Itinerary.vue
+++ src/components/Itinerary.vue
@@ -27,7 +27,7 @@
                   <time class="mb-1 text-sm font-normal leading-none text-white">1.00 PM - 2.00 PM</time>
                   <h3 class="text-lg font-semibold text-orange-500">Platinum Career Talk - NI Malaysia</h3>
                   <p class="mb-4 text-base font-thin text-white">Represented by Selvan Chinappan, the Vice President of APAC Manufacturing and Managing Director of NI Malaysia</p>
-                  <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
+                  <a href="#" @click.prevent="openModal" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
                     Register Here
                     <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -39,7 +39,7 @@
                   <time class="mb-1 text-sm font-normal leading-none text-white">2.15 PM - 2.45 PM</time>
                   <h3 class="text-lg font-semibold text-orange-500">Gold Career Talk - Intel Corporation</h3>
                   <p class="mb-4 text-base font-thin text-white">Represented by Wong Mei Fong, General Manager of Penang Assembly Test Manufacturing</p>
-                  <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
+                  <a href="#" @click.prevent="openModal" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
                     Register Here
                     <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -51,7 +51,7 @@
                   <time class="mb-1 text-sm font-normal leading-none text-white">3.00 PM - 3.20 PM</time>
                   <h3 class="text-lg font-semibold text-orange-500">Silver Career Talk - Vitrox Technologies</h3>
                   <p class="mb-4 text-base font-thin text-white">Represented by Yeoh Siew Eng, Manager of People Management</p>
-                  <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
+                  <a href="#" @click.prevent="openModal" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
                     Register Here
                     <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -94,7 +94,7 @@
                   <time class="mb-1 text-sm font-normal leading-none text-white">1.30 PM - 2.00 PM</time>
                   <h3 class="text-lg font-semibold text-orange-500">Gold Career Talk - Micro Modular System</h3>
                   <p class="mb-4 text-base font-thin text-white">Represented by Joel Nathanael, Operation Director</p>
-                  <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
+                  <a href="#" @click.prevent="openModal" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
                     Register Here
                     <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -106,7 +106,7 @@
                   <time class="mb-1 text-sm font-normal leading-none text-white">2.15 PM - 2.35 PM</time>
                   <h3 class="text-lg font-semibold text-orange-500">Silver Career Talk - Veecotech</h3>
                   <p class="mb-4 text-base font-thin text-white">Represented by Alain Lye, Managing Director</p>
-                  <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
+                  <a href="#" @click.prevent="openModal" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
                     Register Here
                     <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -118,7 +118,7 @@
                   <time class="mb-1 text-sm font-normal leading-none text-white">2.45 PM - 3.00 PM</time>
                   <h3 class="text-lg font-semibold text-orange-500">Bronze Career Talk - Jabil Global Business Center</h3>
                   <p class="mb-4 text-base font-thin text-white">Represented by Rachael Teoh, Project Manager (University Relations & College Recruitment)</p>
-                  <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
+                  <a href="#" @click.prevent="openModal" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-transparent rounded-lg hover:bg-transparent hover:border-white hover:text-white">
                     Register Here
                     <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -188,7 +188,7 @@ export default {
     openModal() {
       if (this.isUserLoggedIn()) {
         this.modalTitle = "RSVP Now";
-        this.modalText = "Are you confirm to join the talk?";
+        this.modalText = "Confirm to join the talk?";
         this.modalColor = "black";
         this.isModalVisible = true;
       } 
